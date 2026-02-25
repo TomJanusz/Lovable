6df5d1fd6d20a9818a62e82a805fc014250a2ae6
Tomasz Januszewski <tom.janusz24800@gmail.com>
2026-02-25 10:14:28 +0100

    Add change in files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-//Tes
+//TesT
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
